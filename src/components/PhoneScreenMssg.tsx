48a6cd33709887b136f42fb4c3aea22ee4ca0871
Harsh Suvarna <harsh.suvarna0@gmail.com>
2024-11-18 13:42:47 +0000

    removed metrics form showing on phone screens

--- src/components/PhoneScreenMssg.tsx
+++ src/components/PhoneScreenMssg.tsx
@@ -2,9 +2,8 @@ import { useEffect, useState } from "react";
 import "../App.css";
 
 function PhoneScreenMssg() {
-  const windowDimensions = window.innerWidth;
-  const show = windowDimensions < 878;
   const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+  const show = windowWidth < 878;
 
   useEffect(() => {
     function handleResize() {
@@ -20,7 +19,6 @@ function PhoneScreenMssg() {
       style={{ visibility: show ? "visible" : "hidden" }}
       className="phone-screen-mssg"
     >
-      {windowWidth}
       <p>Please use Cybersafe on a bigger screen for best Experience.</p>
     </div>
   );
